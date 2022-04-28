f0be9759ec2bbd80a6474958b6154660a35a4e4d
miguel javela <mjavela@compuamerica.com.ve>
2022-04-28 17:54:05 -0500

    clase 15 - duck typing en Javascript

--- main2.js
+++ main2.js
@@ -2,6 +2,40 @@ function requiredParam (param){
     throw new Error(param + " es obligatorio");
 }
 
+function isArray(subject){
+    return Array.isArray(subject);
+}
+
+function createlearningPath ({
+    name = requiredParam("name"),
+    courses = [],
+}){
+    const private = {
+        "_name": name,
+        "_courses": courses,
+    };
+
+    const public = {
+        get name(){
+            return private["_name"];
+        },
+        set name (newName){
+            if(newName.length != 0){
+                private["_name"] = newName;
+            } else {
+                console.warn("tu nombre debe tener al menos 1 caracter")
+            }
+             
+        },
+        get courses(){
+            return private["_courses"];
+        },
+
+    };
+
+    return public;
+}
+
 function createStudent({
     name = requiredParam("name"),
     age,
@@ -13,43 +47,71 @@ function createStudent({
     learningPaths = []
 } = {}){
     const private = {
-        "_name": name
+        "_name": name,
+        "_learningPaths": learningPaths
     };
     const public = {
         email,
         age,
         approvedCourses,
-        learningPaths,
         socialMedia: {
             twitter,
             instagram,
             facebook
         },
-        readName(){
+        get name(){
             return private["_name"];
         },
-        changeName(newName){
-            private._name = newName;
+        set name (newName){
+            if(newName.length != 0){
+                private["_name"] = newName;
+            } else {
+                console.warn("tu nombre debe tener al menos 1 caracter")
+            }
+             
+        },
+        get learningPaths(){
+            return private["_learningPaths"];
+        },
+        set learningPaths (newLp){
+            if(!newLp.name){
+                console.warn("tu LP no tiene nombre");
+                return;
+            }
+            if(!newLp.courses){
+                console.warn ("Tu LP no tiene courses")
+                return;
+            }
+            if(!isArray(newLp.courses)){
+                console.warn ("tu Lp no es una lisa de cursos")
+                return;
+            }
+            private["_learningPaths"].push(newLp);             
         }
-        
+        // readName(){
+        //     return private["_name"];
+        // },
+        // changeName(newName){
+        //     private._name = newName;
+        // }     
     }
     //esto evitaria aplicar polimorfismo
-    Object.defineProperty(public,"readName",{
-        writable:false,
-        configurable:false
-    });
-    Object.defineProperty(public,"changeName",{
-        writable:false,
-        configurable:false
-    });
+    // Object.defineProperty(public,"readName",{
+    //     writable:false,
+    //     configurable:false
+    // });
+    // Object.defineProperty(public,"changeName",{
+    //     writable:false,
+    //     configurable:false
+    // });
     return public;
 }
 
-const data = createStudent();
+//const data = createStudent();
 
-// const dana = createStudent({
-//     name: "juanito",
-//     age: 18,
-//     email: "jj",
-//     twitter: "migu"
-// });
+const dana = createStudent({
+    name: "dana",
+    age: 18,
+    email: "jj",
+    twitter: "migu"
+});
