0e47d221be12ebcf1b38df57e6c9a95a0971bda4
miguel javela <mjavela@compuamerica.com.ve>
2022-04-28 21:58:31 -0500

    clase 18 - creando metodos estaticos en JavaScript

--- main2.js
+++ main2.js
@@ -158,5 +158,39 @@ const dana = new Student({
     ]
 });
 
+class SuperObject {
+    static deepCopy (subject){
+        let copySubject;
+        const subjectIsObject = isObject(subject);
+        const subjectIsArray = isArray(subject);
+    
+        if(subjectIsArray){
+            copySubject = [];
+        } else if (subjectIsObject){
+            copySubject = {};
+        } else {
+            return subject;
+        }
+    
+        for(key in subject){
+            const keyIsObject = isObject(subject[key]);
+    
+            if(keyIsObject){
+                copySubject[key] = deepCopy(subject[key]);
+            } else {
+                if (subjectIsArray){
+                    copySubject.push(subject[key])
+                } else {
+                    copySubject[key] = subject[key];
+                }
+            }
+        }
+        return copySubject;
+    }
+}
 
-
+//crear un metodo estatico con prototipos
+function SuperObject1 (){}
+SuperObject1.isObject = function (){
+    return typeof subject == "object";
+}
